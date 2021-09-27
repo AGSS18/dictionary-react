35180d848bcd9ad1362f2531fc82a87b45a1ce7d
Gina Sala <georgina.sala18@gmail.com>
2021-09-26 22:38:53 -0500

    Update Images.js

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,18 +1,9 @@
 import classes from './Images.module.css';
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import Loader from "react-loader-spinner";
 
 function Images(props) {
     if(props.data.length) {
         return(
             <div className={classes.container}>
-                <Loader
-                    type="Puff"
-                    color="#00BFFF"
-                    height={100}
-                    width={100}
-                    timeout={3000} //3 secs
-                />
                 {props.data.map((image, index) => {
                     return(
                         <a 
